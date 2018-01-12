02706b710349e2cfca8d111fc727b8c694710d28
SHIRLEY ANA OVANDO PAZ <37024731818@jpmorgan.sp>
2018-01-12 21:58:49 -0200

    totalProduto atualizado

--- js/carrinho.js
+++ js/carrinho.js
@@ -1,40 +1,40 @@
 //dados reais viriam do back end (banco de dados)
 var produtos = [
     {//0
-        codigo : 50,
-        imagem : {
-            src     : './img/camiseta.png',
-            alt     : "texto alternativo",
-            title   : "título da imagem"
+        codigo: 50,
+        imagem: {
+            src: './img/camiseta.png',
+            alt: "texto alternativo",
+            title: "título da imagem"
         },
-        descricao    : 'Puma Golf Raglan Tech Polo Tee',
-        cor         : 'Purple',
-        preco       : 72,
-        qtd         : 3
+        descricao: 'Puma Golf Raglan Tech Polo Tee',
+        cor: 'Purple',
+        preco: 72,
+        qtd: 3,
     },
     {//1
-        codigo : 99,
-        imagem : {
-            src     : './img/camiseta.png',
-            alt     : "texto alternativo",
-            title   : "título da imagem"
+        codigo: 99,
+        imagem: {
+            src: './img/camiseta.png',
+            alt: "texto alternativo",
+            title: "título da imagem"
         },
-        descricao    : 'Nike Mens Running Shoe',
-        cor         : 'White',
-        preco       : 50,
-        qtd         : 1
+        descricao: 'Nike Mens Running Shoe',
+        cor: 'White',
+        preco: 50,
+        qtd: 1
     },
     {//2
-        codigo : 287,
-        imagem : {
-            src     : './img/camiseta.png',
-            alt     : "texto alternativo",
-            title   : "título da imagem"
+        codigo: 287,
+        imagem: {
+            src: './img/camiseta.png',
+            alt: "texto alternativo",
+            title: "título da imagem"
         },
-        descricao    : 'DC Mens Axis Snowboard Jacket',
-        cor         : 'White',
-        preco       : 89.97,
-        qtd         : 2
+        descricao: 'DC Mens Axis Snowboard Jacket',
+        cor: 'White',
+        preco: 89.97,
+        qtd: 2
     },
 ]
 
@@ -49,38 +49,48 @@ var produtos = [
         <span id="v1" class="product__value"></span>*<input type="number" name="" id=""> = <span>$216</span>
     </div>
  */
-function listarProdutos(){
- //Gerar o template através do array de objetos
+function listarProdutos() {
+    //Gerar o template através do array de objetos
     var template = "";
-    var total = 0;
-        
-    for(var i = 0; i < produtos.length; i++){
-        console.log( produtos[i] );
+    var totalGeral = 0;
 
-        var subtotal = multiplicar(produtos[i].preco, produtos[i].qtd);
-        total = total + subtotal;
+    for (var i = 0; i < produtos.length; i++) {
+        console.log(produtos[i]);
+
+        var totalProduto = produtos[i].preco * produtos[i].qtd;
+        totalGeral += totalProduto;
 
         template += '<div class="product">';
-        template +=     '<i class="fa fa-times-circle"></i>';
-        template +=     '<img src="' +produtos[i].imagem.src+ '" alt="' +produtos[i].imagem.alt+ '" class="product__img">';
-        template +=     '<span id="p1" class="product__name">'+produtos[i].descricao+'</span>';
-        template +=     '<span id="c1" class="product__color">'+produtos[i].cor+'</span>';
-        template +=     '<span id="s1" class="product__size"></span>';
-        template +=     '<span id="v1" class="product__value";"><strong>R$ </strong>'+produtos[i].preco+'</span>*<input type="number";" value="'+produtos[i].qtd+'" name="" id=""> = <span> <strong>R$ </strong>'+subtotal+'</span>';
+        template += '<i class="fa fa-times-circle"></i>';
+        template += '<img src="' + produtos[i].imagem.src + '" alt="' + produtos[i].imagem.alt + '" class="product__img">';
+        template += '<span id="p1" class="product__name">' + produtos[i].descricao + '</span>';
+        template += '<span id="c1" class="product__color">' + produtos[i].cor + '</span>';
+        template += '<span id="s1" class="product__size"></span>';
+        template += '<span id="v1" class="product__value"><strong>R$ </strong>' + produtos[i].preco + '</span><input type="number" onchange="atualizarQtd(this.id, this.value)" value="' + produtos[i].qtd + '"id="' + produtos[i].codigo + '" name="" id=""> = <span id="total-' + produtos[i].codigo + '"> <strong>R$ </strong>' + totalProduto + '</span>';
         template += '</div>';
     }
 
     document.getElementById("products").innerHTML = template;
-    document.getElementById("totalcart").innerHTML = '<p>Subtotal:'+total+'</p>';
+    document.getElementById("totalcart").innerHTML = '<p>Subtotal:<strong>R$ ' + totalGeral + '</strong></p>';
 }
 
-function multiplicar (_preco, _quantidade) {
-    return _preco*_quantidade;
+function atualizarQtd(_codigo, _qtd) {
+    for (var index = 0; index < produtos.length; index++) {
+        if (produtos[index].codigo == _codigo) {
+            //atualizar a quantidade
+            produtos[index].qtd = _qtd;
+            //atualizar totalproduto
+            var totalProduto = produtos[index].qtd * produtos[index].preco;
+            var codProd = 'total-' + produtos[index].codigo;
+            document.getElementById(codProd).innerHTML = "<strong>R$" + totalProduto + "</strong>";          
+            //atualizar totalGeral
+        console.log(produtos[index]);
+        break;
+        }
+    }
 }
 
-function validaSexo(_id){
-    alert(_id);
-}
+
 
 
 
